23263c61438027a92e0b785b5e1c3d92b571a771
DimaShunko <100087856+DimaShunko@users.noreply.github.com>
2022-06-11 18:36:57 +0300

    Delete DB directory

--- DB/src/main/java/Command.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public interface Command {
-    String getName();
-    void exec(Statement statement) throws SQLException;
-    String help();
-}
